36ae519664771dd6c375e501ee4dd0c2c577bb6a
lxl <xianglong.luo@rancher.cn>
2019-11-22 23:45:20 -0800

    Update the code by go generate after add ApiUrl into wechatconfig

--- client/management/v3/zz_generated_wechat_config.go
+++ client/management/v3/zz_generated_wechat_config.go
@@ -3,6 +3,7 @@ package client
 const (
 	WechatConfigType                  = "wechatConfig"
 	WechatConfigFieldAgent            = "agent"
+	WechatConfigFieldApiUrl           = "apiUrl"
 	WechatConfigFieldCorp             = "corp"
 	WechatConfigFieldDefaultRecipient = "defaultRecipient"
 	WechatConfigFieldProxyURL         = "proxyUrl"
@@ -12,6 +13,7 @@ const (
 
 type WechatConfig struct {
 	Agent            string `json:"agent,omitempty" yaml:"agent,omitempty"`
+	ApiUrl           string `json:"apiUrl,omitempty" yaml:"apiUrl,omitempty"`
 	Corp             string `json:"corp,omitempty" yaml:"corp,omitempty"`
 	DefaultRecipient string `json:"defaultRecipient,omitempty" yaml:"defaultRecipient,omitempty"`
 	ProxyURL         string `json:"proxyUrl,omitempty" yaml:"proxyUrl,omitempty"`
