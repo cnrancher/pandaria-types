860dadff0fa09f58ab57fed38f6e7ff1ce3c77d0
Jason-ZW <zhenyang@rancher.com>
2019-12-13 15:21:11 +0800

    PANDARIA: add GPU quota types
    Signed-off-by: Jason-ZW <zhenyang@rancher.com>

--- apis/management.cattle.io/v3/resource_quota_types.go
+++ apis/management.cattle.io/v3/resource_quota_types.go
@@ -23,6 +23,8 @@ type ResourceQuotaLimit struct {
 	RequestsStorage        string `json:"requestsStorage,omitempty"`
 	LimitsCPU              string `json:"limitsCpu,omitempty"`
 	LimitsMemory           string `json:"limitsMemory,omitempty"`
+	RequestsGPUMemory      string `json:"requestsGpuMemory,omitempty"`
+	RequestsGPUCount       string `json:"requestsGpuCount,omitempty"`
 }
 
 type ContainerResourceLimit struct {
