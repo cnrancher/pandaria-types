0278d4167029485d689f03e9313b3d46cb0fa3a3
NewGr8Player <273221594@qq.com>
2020-04-14 05:28:21 +0000

    PANDARIA:go generate

--- client/management/v3/zz_generated_slack_config.go
+++ client/management/v3/zz_generated_slack_config.go
@@ -2,12 +2,14 @@ package client
 
 const (
 	SlackConfigType                  = "slackConfig"
+	SlackConfigFieldBearerToken      = "bearer_token"
 	SlackConfigFieldDefaultRecipient = "defaultRecipient"
 	SlackConfigFieldProxyURL         = "proxyUrl"
 	SlackConfigFieldURL              = "url"
 )
 
 type SlackConfig struct {
+	BearerToken      string `json:"bearer_token,omitempty" yaml:"bearer_token,omitempty"`
 	DefaultRecipient string `json:"defaultRecipient,omitempty" yaml:"defaultRecipient,omitempty"`
 	ProxyURL         string `json:"proxyUrl,omitempty" yaml:"proxyUrl,omitempty"`
 	URL              string `json:"url,omitempty" yaml:"url,omitempty"`
