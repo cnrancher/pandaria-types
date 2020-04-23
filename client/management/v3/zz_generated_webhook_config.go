13df016fa5d09573bfd3b32028ed4b841773903b
NewGr8Player <273221594@qq.com>
2020-04-23 04:14:26 +0000

    PANDARIA:go generate

--- client/management/v3/zz_generated_webhook_config.go
+++ client/management/v3/zz_generated_webhook_config.go
@@ -2,13 +2,15 @@ package client
 
 const (
 	WebhookConfigType             = "webhookConfig"
+	WebhookConfigFieldBasicAuth   = "basic_auth"
 	WebhookConfigFieldBearerToken = "bearer_token"
 	WebhookConfigFieldProxyURL    = "proxyUrl"
 	WebhookConfigFieldURL         = "url"
 )
 
 type WebhookConfig struct {
-	BearerToken string `json:"bearer_token,omitempty" yaml:"bearer_token,omitempty"`
-	ProxyURL    string `json:"proxyUrl,omitempty" yaml:"proxyUrl,omitempty"`
-	URL         string `json:"url,omitempty" yaml:"url,omitempty"`
+	BasicAuth   *BasicAuth `json:"basic_auth,omitempty" yaml:"basic_auth,omitempty"`
+	BearerToken string     `json:"bearer_token,omitempty" yaml:"bearer_token,omitempty"`
+	ProxyURL    string     `json:"proxyUrl,omitempty" yaml:"proxyUrl,omitempty"`
+	URL         string     `json:"url,omitempty" yaml:"url,omitempty"`
 }
