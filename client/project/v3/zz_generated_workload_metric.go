7c6747d4da9b0993e7190d1b9848e1624fdc83ed
michelia feng <michelia.feng@gmail.com>
2020-07-14 16:44:40 +0800

    PANDARIA: generate code

--- client/project/v3/zz_generated_workload_metric.go
+++ client/project/v3/zz_generated_workload_metric.go
@@ -1,14 +1,16 @@
 package client
 
 const (
-	WorkloadMetricType        = "workloadMetric"
-	WorkloadMetricFieldPath   = "path"
-	WorkloadMetricFieldPort   = "port"
-	WorkloadMetricFieldSchema = "schema"
+	WorkloadMetricType                             = "workloadMetric"
+	WorkloadMetricFieldPath                        = "path"
+	WorkloadMetricFieldPort                        = "port"
+	WorkloadMetricFieldSchema                      = "schema"
+	WorkloadMetricFieldWorkloadMetricRelabelConfig = "workloadMetricRelabelConfig"
 )
 
 type WorkloadMetric struct {
-	Path   string `json:"path,omitempty" yaml:"path,omitempty"`
-	Port   int64  `json:"port,omitempty" yaml:"port,omitempty"`
-	Schema string `json:"schema,omitempty" yaml:"schema,omitempty"`
+	Path                        string                        `json:"path,omitempty" yaml:"path,omitempty"`
+	Port                        int64                         `json:"port,omitempty" yaml:"port,omitempty"`
+	Schema                      string                        `json:"schema,omitempty" yaml:"schema,omitempty"`
+	WorkloadMetricRelabelConfig []WorkloadMetricRelabelConfig `json:"workloadMetricRelabelConfig,omitempty" yaml:"workloadMetricRelabelConfig,omitempty"`
 }
