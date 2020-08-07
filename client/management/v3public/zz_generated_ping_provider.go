20b2f38ca8b2f9a4a9b61110e073767e4e1da594
NewGr8Player <273221594@qq.com>
2020-08-07 03:28:22 +0000

    PANDARIA: go generate

--- client/management/v3public/zz_generated_ping_provider.go
+++ client/management/v3public/zz_generated_ping_provider.go
@@ -6,6 +6,7 @@ const (
 	PingProviderFieldCreated         = "created"
 	PingProviderFieldCreatorID       = "creatorId"
 	PingProviderFieldLabels          = "labels"
+	PingProviderFieldLogoutURL       = "logoutUrl"
 	PingProviderFieldName            = "name"
 	PingProviderFieldOwnerReferences = "ownerReferences"
 	PingProviderFieldRedirectURL     = "redirectUrl"
@@ -19,6 +20,7 @@ type PingProvider struct {
 	Created         string            `json:"created,omitempty" yaml:"created,omitempty"`
 	CreatorID       string            `json:"creatorId,omitempty" yaml:"creatorId,omitempty"`
 	Labels          map[string]string `json:"labels,omitempty" yaml:"labels,omitempty"`
+	LogoutURL       string            `json:"logoutUrl,omitempty" yaml:"logoutUrl,omitempty"`
 	Name            string            `json:"name,omitempty" yaml:"name,omitempty"`
 	OwnerReferences []OwnerReference  `json:"ownerReferences,omitempty" yaml:"ownerReferences,omitempty"`
 	RedirectURL     string            `json:"redirectUrl,omitempty" yaml:"redirectUrl,omitempty"`
