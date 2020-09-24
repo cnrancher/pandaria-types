9c1ffca48c4dfe93683a05c75bbdcf1bca731dac
GGGitBoy <1812041288@qq.com>
2020-09-24 15:07:26 +0800

    PANDARIA: go generate

--- client/management/v3/zz_generated_monitoring_input.go
+++ client/management/v3/zz_generated_monitoring_input.go
@@ -1,12 +1,14 @@
 package client
 
 const (
-	MonitoringInputType         = "monitoringInput"
-	MonitoringInputFieldAnswers = "answers"
-	MonitoringInputFieldVersion = "version"
+	MonitoringInputType            = "monitoringInput"
+	MonitoringInputFieldAnswers    = "answers"
+	MonitoringInputFieldValuesYaml = "valuesYaml"
+	MonitoringInputFieldVersion    = "version"
 )
 
 type MonitoringInput struct {
-	Answers map[string]string `json:"answers,omitempty" yaml:"answers,omitempty"`
-	Version string            `json:"version,omitempty" yaml:"version,omitempty"`
+	Answers    map[string]string `json:"answers,omitempty" yaml:"answers,omitempty"`
+	ValuesYaml string            `json:"valuesYaml,omitempty" yaml:"valuesYaml,omitempty"`
+	Version    string            `json:"version,omitempty" yaml:"version,omitempty"`
 }
