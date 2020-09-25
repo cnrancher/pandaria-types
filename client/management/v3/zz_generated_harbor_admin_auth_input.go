e63246e872fada33f59c721e7f51f775f2332065
smallteehs <503630985@qq.com>
2020-09-25 17:21:41 +0800

    PANDARIA: add Version to HarborAdminAuthInput for Harbor2.0

--- client/management/v3/zz_generated_harbor_admin_auth_input.go
+++ client/management/v3/zz_generated_harbor_admin_auth_input.go
@@ -5,10 +5,12 @@ const (
 	HarborAdminAuthInputFieldPassword  = "password"
 	HarborAdminAuthInputFieldServerURL = "serverURL"
 	HarborAdminAuthInputFieldUsername  = "username"
+	HarborAdminAuthInputFieldVersion   = "version"
 )
 
 type HarborAdminAuthInput struct {
 	Password  string `json:"password,omitempty" yaml:"password,omitempty"`
 	ServerURL string `json:"serverURL,omitempty" yaml:"serverURL,omitempty"`
 	Username  string `json:"username,omitempty" yaml:"username,omitempty"`
+	Version   string `json:"version,omitempty" yaml:"version,omitempty"`
 }
