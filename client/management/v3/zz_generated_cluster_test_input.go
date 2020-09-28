d51b9d73455079c358670dd585ba8ca55fe632be
GGGitBoy <1812041288@qq.com>
2020-09-28 16:11:02 +0800

    PANDARIA: go generate

--- client/management/v3/zz_generated_cluster_test_input.go
+++ client/management/v3/zz_generated_cluster_test_input.go
@@ -1,24 +1,38 @@
 package client
 
 const (
-	ClusterTestInputType                       = "clusterTestInput"
-	ClusterTestInputFieldClusterName           = "clusterId"
-	ClusterTestInputFieldCustomTargetConfig    = "customTargetConfig"
-	ClusterTestInputFieldElasticsearchConfig   = "elasticsearchConfig"
-	ClusterTestInputFieldFluentForwarderConfig = "fluentForwarderConfig"
-	ClusterTestInputFieldKafkaConfig           = "kafkaConfig"
-	ClusterTestInputFieldOutputTags            = "outputTags"
-	ClusterTestInputFieldSplunkConfig          = "splunkConfig"
-	ClusterTestInputFieldSyslogConfig          = "syslogConfig"
+	ClusterTestInputType                           = "clusterTestInput"
+	ClusterTestInputFieldClusterName               = "clusterId"
+	ClusterTestInputFieldCustomTargetConfig        = "customTargetConfig"
+	ClusterTestInputFieldDisplayName               = "displayName"
+	ClusterTestInputFieldElasticsearchConfig       = "elasticsearchConfig"
+	ClusterTestInputFieldEnableExceptionStackMatch = "enableExceptionStackMatch"
+	ClusterTestInputFieldEnableJSONParsing         = "enableJSONParsing"
+	ClusterTestInputFieldEnableMultiLineFilter     = "enableMultiLineFilter"
+	ClusterTestInputFieldFluentForwarderConfig     = "fluentForwarderConfig"
+	ClusterTestInputFieldKafkaConfig               = "kafkaConfig"
+	ClusterTestInputFieldMultiLineEndRegexp        = "multiLineEndRegexp"
+	ClusterTestInputFieldMultiLineStartRegexp      = "multiLineStartRegexp"
+	ClusterTestInputFieldOutputFlushInterval       = "outputFlushInterval"
+	ClusterTestInputFieldOutputTags                = "outputTags"
+	ClusterTestInputFieldSplunkConfig              = "splunkConfig"
+	ClusterTestInputFieldSyslogConfig              = "syslogConfig"
 )
 
 type ClusterTestInput struct {
-	ClusterName           string                 `json:"clusterId,omitempty" yaml:"clusterId,omitempty"`
-	CustomTargetConfig    *CustomTargetConfig    `json:"customTargetConfig,omitempty" yaml:"customTargetConfig,omitempty"`
-	ElasticsearchConfig   *ElasticsearchConfig   `json:"elasticsearchConfig,omitempty" yaml:"elasticsearchConfig,omitempty"`
-	FluentForwarderConfig *FluentForwarderConfig `json:"fluentForwarderConfig,omitempty" yaml:"fluentForwarderConfig,omitempty"`
-	KafkaConfig           *KafkaConfig           `json:"kafkaConfig,omitempty" yaml:"kafkaConfig,omitempty"`
-	OutputTags            map[string]string      `json:"outputTags,omitempty" yaml:"outputTags,omitempty"`
-	SplunkConfig          *SplunkConfig          `json:"splunkConfig,omitempty" yaml:"splunkConfig,omitempty"`
-	SyslogConfig          *SyslogConfig          `json:"syslogConfig,omitempty" yaml:"syslogConfig,omitempty"`
+	ClusterName               string                 `json:"clusterId,omitempty" yaml:"clusterId,omitempty"`
+	CustomTargetConfig        *CustomTargetConfig    `json:"customTargetConfig,omitempty" yaml:"customTargetConfig,omitempty"`
+	DisplayName               string                 `json:"displayName,omitempty" yaml:"displayName,omitempty"`
+	ElasticsearchConfig       *ElasticsearchConfig   `json:"elasticsearchConfig,omitempty" yaml:"elasticsearchConfig,omitempty"`
+	EnableExceptionStackMatch bool                   `json:"enableExceptionStackMatch,omitempty" yaml:"enableExceptionStackMatch,omitempty"`
+	EnableJSONParsing         bool                   `json:"enableJSONParsing,omitempty" yaml:"enableJSONParsing,omitempty"`
+	EnableMultiLineFilter     bool                   `json:"enableMultiLineFilter,omitempty" yaml:"enableMultiLineFilter,omitempty"`
+	FluentForwarderConfig     *FluentForwarderConfig `json:"fluentForwarderConfig,omitempty" yaml:"fluentForwarderConfig,omitempty"`
+	KafkaConfig               *KafkaConfig           `json:"kafkaConfig,omitempty" yaml:"kafkaConfig,omitempty"`
+	MultiLineEndRegexp        string                 `json:"multiLineEndRegexp,omitempty" yaml:"multiLineEndRegexp,omitempty"`
+	MultiLineStartRegexp      string                 `json:"multiLineStartRegexp,omitempty" yaml:"multiLineStartRegexp,omitempty"`
+	OutputFlushInterval       int64                  `json:"outputFlushInterval,omitempty" yaml:"outputFlushInterval,omitempty"`
+	OutputTags                map[string]string      `json:"outputTags,omitempty" yaml:"outputTags,omitempty"`
+	SplunkConfig              *SplunkConfig          `json:"splunkConfig,omitempty" yaml:"splunkConfig,omitempty"`
+	SyslogConfig              *SyslogConfig          `json:"syslogConfig,omitempty" yaml:"syslogConfig,omitempty"`
 }
--- client/management/v3/zz_generated_project_test_input.go
+++ client/management/v3/zz_generated_project_test_input.go
@@ -1,24 +1,38 @@
 package client
 
 const (
-	ProjectTestInputType                       = "projectTestInput"
-	ProjectTestInputFieldCustomTargetConfig    = "customTargetConfig"
-	ProjectTestInputFieldElasticsearchConfig   = "elasticsearchConfig"
-	ProjectTestInputFieldFluentForwarderConfig = "fluentForwarderConfig"
-	ProjectTestInputFieldKafkaConfig           = "kafkaConfig"
-	ProjectTestInputFieldOutputTags            = "outputTags"
-	ProjectTestInputFieldProjectName           = "projectId"
-	ProjectTestInputFieldSplunkConfig          = "splunkConfig"
-	ProjectTestInputFieldSyslogConfig          = "syslogConfig"
+	ProjectTestInputType                           = "projectTestInput"
+	ProjectTestInputFieldCustomTargetConfig        = "customTargetConfig"
+	ProjectTestInputFieldDisplayName               = "displayName"
+	ProjectTestInputFieldElasticsearchConfig       = "elasticsearchConfig"
+	ProjectTestInputFieldEnableExceptionStackMatch = "enableExceptionStackMatch"
+	ProjectTestInputFieldEnableJSONParsing         = "enableJSONParsing"
+	ProjectTestInputFieldEnableMultiLineFilter     = "enableMultiLineFilter"
+	ProjectTestInputFieldFluentForwarderConfig     = "fluentForwarderConfig"
+	ProjectTestInputFieldKafkaConfig               = "kafkaConfig"
+	ProjectTestInputFieldMultiLineEndRegexp        = "multiLineEndRegexp"
+	ProjectTestInputFieldMultiLineStartRegexp      = "multiLineStartRegexp"
+	ProjectTestInputFieldOutputFlushInterval       = "outputFlushInterval"
+	ProjectTestInputFieldOutputTags                = "outputTags"
+	ProjectTestInputFieldProjectName               = "projectId"
+	ProjectTestInputFieldSplunkConfig              = "splunkConfig"
+	ProjectTestInputFieldSyslogConfig              = "syslogConfig"
 )
 
 type ProjectTestInput struct {
-	CustomTargetConfig    *CustomTargetConfig    `json:"customTargetConfig,omitempty" yaml:"customTargetConfig,omitempty"`
-	ElasticsearchConfig   *ElasticsearchConfig   `json:"elasticsearchConfig,omitempty" yaml:"elasticsearchConfig,omitempty"`
-	FluentForwarderConfig *FluentForwarderConfig `json:"fluentForwarderConfig,omitempty" yaml:"fluentForwarderConfig,omitempty"`
-	KafkaConfig           *KafkaConfig           `json:"kafkaConfig,omitempty" yaml:"kafkaConfig,omitempty"`
-	OutputTags            map[string]string      `json:"outputTags,omitempty" yaml:"outputTags,omitempty"`
-	ProjectName           string                 `json:"projectId,omitempty" yaml:"projectId,omitempty"`
-	SplunkConfig          *SplunkConfig          `json:"splunkConfig,omitempty" yaml:"splunkConfig,omitempty"`
-	SyslogConfig          *SyslogConfig          `json:"syslogConfig,omitempty" yaml:"syslogConfig,omitempty"`
+	CustomTargetConfig        *CustomTargetConfig    `json:"customTargetConfig,omitempty" yaml:"customTargetConfig,omitempty"`
+	DisplayName               string                 `json:"displayName,omitempty" yaml:"displayName,omitempty"`
+	ElasticsearchConfig       *ElasticsearchConfig   `json:"elasticsearchConfig,omitempty" yaml:"elasticsearchConfig,omitempty"`
+	EnableExceptionStackMatch bool                   `json:"enableExceptionStackMatch,omitempty" yaml:"enableExceptionStackMatch,omitempty"`
+	EnableJSONParsing         bool                   `json:"enableJSONParsing,omitempty" yaml:"enableJSONParsing,omitempty"`
+	EnableMultiLineFilter     bool                   `json:"enableMultiLineFilter,omitempty" yaml:"enableMultiLineFilter,omitempty"`
+	FluentForwarderConfig     *FluentForwarderConfig `json:"fluentForwarderConfig,omitempty" yaml:"fluentForwarderConfig,omitempty"`
+	KafkaConfig               *KafkaConfig           `json:"kafkaConfig,omitempty" yaml:"kafkaConfig,omitempty"`
+	MultiLineEndRegexp        string                 `json:"multiLineEndRegexp,omitempty" yaml:"multiLineEndRegexp,omitempty"`
+	MultiLineStartRegexp      string                 `json:"multiLineStartRegexp,omitempty" yaml:"multiLineStartRegexp,omitempty"`
+	OutputFlushInterval       int64                  `json:"outputFlushInterval,omitempty" yaml:"outputFlushInterval,omitempty"`
+	OutputTags                map[string]string      `json:"outputTags,omitempty" yaml:"outputTags,omitempty"`
+	ProjectName               string                 `json:"projectId,omitempty" yaml:"projectId,omitempty"`
+	SplunkConfig              *SplunkConfig          `json:"splunkConfig,omitempty" yaml:"splunkConfig,omitempty"`
+	SyslogConfig              *SyslogConfig          `json:"syslogConfig,omitempty" yaml:"syslogConfig,omitempty"`
 }
