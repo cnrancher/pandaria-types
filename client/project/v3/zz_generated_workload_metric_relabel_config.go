1a990bb5efcbae752f7eeb93b3ab50cac0b63fb1
michelia <michelia@rancher.com>
2021-01-11 11:56:34 +0800

    PANDARIA: generate code

--- client/project/v3/zz_generated_workload_metric_relabel_config.go
+++ client/project/v3/zz_generated_workload_metric_relabel_config.go
@@ -1,12 +1,20 @@
 package client
 
 const (
-	WorkloadMetricRelabelConfigType        = "workloadMetricRelabelConfig"
-	WorkloadMetricRelabelConfigFieldAction = "action"
-	WorkloadMetricRelabelConfigFieldRegex  = "regex"
+	WorkloadMetricRelabelConfigType              = "workloadMetricRelabelConfig"
+	WorkloadMetricRelabelConfigFieldAction       = "action"
+	WorkloadMetricRelabelConfigFieldRegex        = "regex"
+	WorkloadMetricRelabelConfigFieldRelabelType  = "relabelType"
+	WorkloadMetricRelabelConfigFieldReplacement  = "replacement"
+	WorkloadMetricRelabelConfigFieldSourceLabels = "sourceLabels"
+	WorkloadMetricRelabelConfigFieldTargetLabel  = "targetLabel"
 )
 
 type WorkloadMetricRelabelConfig struct {
-	Action string `json:"action,omitempty" yaml:"action,omitempty"`
-	Regex  string `json:"regex,omitempty" yaml:"regex,omitempty"`
+	Action       string   `json:"action,omitempty" yaml:"action,omitempty"`
+	Regex        string   `json:"regex,omitempty" yaml:"regex,omitempty"`
+	RelabelType  string   `json:"relabelType,omitempty" yaml:"relabelType,omitempty"`
+	Replacement  string   `json:"replacement,omitempty" yaml:"replacement,omitempty"`
+	SourceLabels []string `json:"sourceLabels,omitempty" yaml:"sourceLabels,omitempty"`
+	TargetLabel  string   `json:"targetLabel,omitempty" yaml:"targetLabel,omitempty"`
 }
