fa7f5962ffc57b5048f9904bf5fa4f23e58a75fe
GGGitBoy <1812041288@qq.com>
2021-12-24 15:57:41 +0800

    PANDARIA: go generate

--- client/cluster/v3/zz_generated_container_resource_limit.go
+++ client/cluster/v3/zz_generated_container_resource_limit.go
@@ -4,6 +4,10 @@ const (
 	ContainerResourceLimitType                = "containerResourceLimit"
 	ContainerResourceLimitFieldLimitsCPU      = "limitsCpu"
 	ContainerResourceLimitFieldLimitsMemory   = "limitsMemory"
+	ContainerResourceLimitFieldMaxCPU         = "maxCpu"
+	ContainerResourceLimitFieldMaxMemory      = "maxMemory"
+	ContainerResourceLimitFieldMinCPU         = "minCpu"
+	ContainerResourceLimitFieldMinMemory      = "minMemory"
 	ContainerResourceLimitFieldRequestsCPU    = "requestsCpu"
 	ContainerResourceLimitFieldRequestsMemory = "requestsMemory"
 )
@@ -11,6 +15,10 @@ const (
 type ContainerResourceLimit struct {
 	LimitsCPU      string `json:"limitsCpu,omitempty" yaml:"limitsCpu,omitempty"`
 	LimitsMemory   string `json:"limitsMemory,omitempty" yaml:"limitsMemory,omitempty"`
+	MaxCPU         string `json:"maxCpu,omitempty" yaml:"maxCpu,omitempty"`
+	MaxMemory      string `json:"maxMemory,omitempty" yaml:"maxMemory,omitempty"`
+	MinCPU         string `json:"minCpu,omitempty" yaml:"minCpu,omitempty"`
+	MinMemory      string `json:"minMemory,omitempty" yaml:"minMemory,omitempty"`
 	RequestsCPU    string `json:"requestsCpu,omitempty" yaml:"requestsCpu,omitempty"`
 	RequestsMemory string `json:"requestsMemory,omitempty" yaml:"requestsMemory,omitempty"`
 }
